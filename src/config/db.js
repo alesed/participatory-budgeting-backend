75d9ec70a80d5eac67cb95f3165c27dfe7ee8676
Ales Sedlacek <xsedla1c@vutbr.cz>
2021-03-26 21:36:53 +0100

    environment params changed

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_URL,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
 });
 
 module.exports = pool;
