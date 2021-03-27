c5ece289c1cfabd6a6dc800cb2d632efbc9cf50b
Ales Sedlacek <xsedla1c@vutbr.cz>
2021-03-27 16:26:08 +0100

    db.js modified for production mode

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,24 @@ require("dotenv").config();
 
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_URL,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
-  ssl: process.env.SSL_ENABLE ? true : false,
-});
+let pool;
+
+if (process.env.SSL_ENABLE) {
+  // dev mode
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+} else {
+  // production mode
+  pool = new Pool({
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    ssl: false,
+  });
+}
 
 module.exports = pool;
